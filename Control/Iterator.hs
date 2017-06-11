f4c48f79cbe3e322e65f9f77cf06c21cbf21d952
PeterMilley <peter.milley@gmail.com>
2017-06-10 23:19:22 -0400

    More doc comments, explicit exports, and so forth.

--- Control/Iterator.hs
+++ Control/Iterator.hs
@@ -17,6 +17,33 @@ module Control.Iterator
   , runIterator
   ) where
 
+{-|
+Module      : Control.Iterator
+Description : Co-operative asymmetric co-routines.
+
+This is a co-routine implementation based on Edward Kmett's "Free Monads for
+Less" series of blog posts, specifically the third. 'YieldT' and 'IteratorT'
+are both free monad transformers which extend their inner monad with the
+'yield' method:
+@
+    yield :: o -> m i
+@
+which suspends computation, yielding a value of type @o@ to the controlling
+function and waiting to be resumed with a value of type @i@.
+
+The two monads differ in how the controlling function drives the co-routine.
+'YieldT' admits 'stepM' which performs a single step of the computation,
+stopping at the first 'yield' or when the co-routine completes. 'IteratorT'
+admits 'runIteratorM' which runs the co-routine to completion given suitable
+continuations. The two types of co-routine can be converted back and forth.
+
+If you read the aforementioned blog posts, you'll note that nested 'YieldT'
+co-routines may be inefficient to construct (in pretty much the same way that
+repeatedly appending to the right of a list is inefficient). If this is a
+concern, construct your co-routines as 'IteratorT' and then convert to
+'YieldT' before first use.
+-}
+
 import Control.Monad.Identity
 import Control.Monad.Trans.Free
 import Control.Monad.Trans.Free.Church
@@ -24,30 +51,42 @@ import Control.Monad.Trans.Free.Church
 data Store i o r = Store (i -> r) o
     deriving (Functor)
 
+-- | A typeclass for functors which can suspend computation, yielding a value
+--   of type @o@, and wait to be resumed with a value of type @i@. Both
+--   @YieldT i o m@ and @IteratorT i o m@ are instances.
 class Functor y => Yieldable y i o | y -> i o where
    yield :: o -> y i
 
 instance Yieldable (Store i o) i o where
   yield = Store id
 
+-- | A monad transformer which makes its inner monad 'Yieldable'.
 type YieldT i o m = FreeT (Store i o) m
 type Yield i o = YieldT i o Identity
 
 instance (Yieldable y i o, Monad m) => Yieldable (FreeT y m) i o where
   yield = liftF . yield
 
+-- | Another monad transformer which makes its inner monad 'Yieldable',
+--   but using Church-encoded free monads (i.e. continuations, lots of
+--   continuations).
 type IteratorT i o m = FT (Store i o) m
 type Iterator i o = IteratorT i o Identity
 
 instance (Yieldable y i o, Monad m) => Yieldable (FT y m) i o where
   yield = liftF . yield
 
+-- | Convert an 'IteratorT' to a 'YieldT'.
 toYield :: Monad m => IteratorT i o m a -> YieldT i o m a
 toYield = fromFT
 
+-- | Convert a 'YieldT' to an 'IteratorT'.
 toIterator :: Monad m => YieldT i o m a -> IteratorT i o m a
 toIterator = toFT
 
+-- | Perform a single step of a 'YieldT', returning 'Either' the final
+--   value of the computation, or an intermediate value and a continuation
+--   to resume the computation.
 stepM :: Monad m => YieldT i o m a -> m (Either a (o, i -> YieldT i o m a))
 stepM y = do
   x <- runFreeT y
@@ -58,6 +97,9 @@ stepM y = do
 step :: Yield i o a -> Either a (o, i -> Yield i o a)
 step = runIdentity . stepM
 
+-- | Run an 'IteratorT' to completion by giving it two continuations, one
+--   for what to do with the final value and one for what to do with both
+--   an intermediate value and a `resume` continuation.
 runIteratorM :: Monad m => IteratorT i o m a
              -> (a -> m r) -> (o -> (i -> m r) -> m r) -> m r
 runIteratorM (FT m) =
--- Control/ReverseState.hs
+++ Control/ReverseState.hs
@@ -1,6 +1,50 @@
 {-# LANGUAGE RecursiveDo #-}
 {-# LANGUAGE TupleSections #-}
-module ReverseState where
+module Control.ReverseState
+  ( RStateT(..)
+  , rstate, rget, rput, rmodify
+  , RState(..)
+  , runRState
+  , evalRState
+  , execRState
+  ) where
+
+{-|
+Module      : Control.ReverseState
+Description : A reverse state monad.
+
+This code is honestly entirely cribbed from the rev-state package on hackage,
+but with names that don't collide with the actual State monad because
+that's just confusing.
+
+The reverse state monad is nearly identical to the State monad except the
+state is propagated in the opposite direction to the non-state value. This
+only works due to lazy evaluation.
+
+'RState' is equivalent to the 'fix' function in utility: it can be implemented
+in terms of 'fix', and 'fix' can be redefined in terms of 'RState':
+
+@
+    fix' f = evalRState (do { x <- rget; rput (f x); return x })
+                        (error "unused")
+@
+
+Another example: here's a function which pads each element in a list of
+strings with spaces on the left until they're all the same length, making only
+one pass through the list. The max of the string lengths is computed in
+the state variable via 'rmodify' and thus sent back to the beginning of the
+computation, then 'rget' retrieves the result and sends it forward as the
+first argument to the helper function @go@:
+
+@
+    pad :: [String] -> [String]
+    pad l = evalRState (rget >>= \n -> mapM (go n) l) 0
+      where
+        go n s = let l = length s
+                 in  replicate (n-l) ' ' ++ s <$ rmodify (max l)
+@
+
+-}
 
 import Control.Arrow (first)
 import Control.Monad
@@ -8,7 +52,13 @@ import Control.Monad.Fix
 import Control.Monad.Identity
 import Control.Monad.Trans
 
-newtype RStateT s m a = RStateT { runRStateT :: s -> m (a, s) }
+-- | The reverse state monad `transformer`. Not really a monad transformer
+--   as the result is only a monad if the inner monad @m@ is actually a
+--   'MonadFix'.
+newtype RStateT s m a =
+  RStateT { -- | Run the underlying state computation.
+            runRStateT :: s -> m (a, s)
+          }
 
 instance Functor m => Functor (RStateT s m) where
   fmap f m = RStateT (\s -> first f <$> runRStateT m s)
@@ -31,25 +81,34 @@ instance MonadFix m => MonadFix (RStateT s m) where
 instance MonadTrans (RStateT s) where
   lift m = RStateT (\s -> fmap (,s) m)
 
+-- | Construct a monad from a pure state function.
 rstate :: Applicative m => (s -> (a, s)) -> RStateT s m a
 rstate f = RStateT (pure . f)
 
+-- | Get the current state, which will be the result of the next _subsequent_
+--   'rput' or 'rmodify'. Essentially, get the state from the future.
 rget :: Applicative m => RStateT s m s
 rget = rstate (\s -> (s, s))
 
+-- | Update the current state, sending the new value backwards.
 rput :: Applicative m => s -> RStateT s m ()
 rput s = rstate (\_ -> ((), s))
 
+-- | Modify the current state, sending the new value backwards.
 rmodify :: Applicative m => (s -> s) -> RStateT s m ()
 rmodify f = rstate (\s -> ((), f s))
 
+-- | A reverse state monad without an inner monad.
 type RState s = RStateT s Identity
 
+-- | Run the underlying state computation.
 runRState :: RState s a -> s -> (a, s)
 runRState m s = runIdentity $ runRStateT m s
 
+-- | Run the state computation, keeping only the final value.
 evalRState :: RState s a -> s -> a
 evalRState m s = fst $ runRState m s
 
+-- | Run the state computation, keeping only the final state.
 execRState :: RState s a -> s -> s
 execRState m s = snd $ runRState m s
